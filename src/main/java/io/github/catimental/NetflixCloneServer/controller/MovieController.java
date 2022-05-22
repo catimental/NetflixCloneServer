9a00d291b2b174cc31250b0dc7835ccb3599c055
catimental <suny970311@gmail.com>
2022-05-22 18:52:14 +0900

    어노테이션 변경, 스트리밍 API 추가
    MovieController의 @Controller어노테이션을 @RestController로 변경
    스트리밍용 API 작업

--- src/main/java/io/github/catimental/NetflixCloneServer/controller/MovieController.java
+++ src/main/java/io/github/catimental/NetflixCloneServer/controller/MovieController.java
@@ -1,19 +1,20 @@
 package io.github.catimental.NetflixCloneServer.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.*;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class MovieController {
 
     @GetMapping(value = "/video/{name}")
@@ -22,6 +23,7 @@ public class MovieController {
         if(!video.exists()) {
             video = new UrlResource("classpath:movies/"+"123"+".mp4");
         }
+
         ResourceRegion resourceRegion;
         final long chunkSize = 1000000L;
         long contentLength = video.contentLength();
@@ -38,9 +40,37 @@ public class MovieController {
             long rangeLength = Long.min(chunkSize, contentLength);
             resourceRegion = new ResourceRegion(video, 0, rangeLength);
         }
+
         return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
                 .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
                 .body(resourceRegion);
 
+    }
+
+    @GetMapping("/region/{fileName}")
+    public ResponseEntity<ResourceRegion> videoRegion(@PathVariable String fileName, @RequestHeader HttpHeaders headers) throws IOException {
+        var resource = new ClassPathResource("movies/%s.mp4".formatted(fileName));
+        if(!resource.exists()) {
+            resource = new ClassPathResource("movies/sample.mp4");
+        }
+
+        final long chunkSize = 1024 * 1024 * 1;
+        long contentLength = resource.contentLength();
+        ResourceRegion region;
+        try {
+            HttpRange httpRange = headers.getRange().stream().findFirst().get();
+            long start = httpRange.getRangeStart(contentLength);
+            long end = httpRange.getRangeEnd(contentLength);
+            long rangeLength = Long.min(chunkSize, end - start + 1);
+            region = new ResourceRegion(resource, start, rangeLength);
+        } catch (Exception e) {
+            long rangeLength = Long.min(chunkSize, contentLength);
+            region = new ResourceRegion(resource, 0, rangeLength);
+        }
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).cacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+                .contentType(MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .header("Accept-Ranges", "bytes")
+                .eTag(fileName) // IE 부분 호출을 위해서 설정
+                .body(region);
     }
 }
